2292b34be0d4f9973378ceed9eed5a0db45d843d
Shaheed100 <68748392+Shaheed100@users.noreply.github.com>
2023-01-19 23:27:11 +0600

    Added passenger counter files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let entryEl = document.getElementById("entry-el");
+let exitEl = document.getElementById("exit-el");
+let totalEl = document.getElementById("totalSum");
+
+function countCheker() {
+  count += 1;
+  countEl.textContent = count; // textContent is better than innerText
+}
+
+function entry() {
+  let countPlus = count + " + ";
+  entryEl.textContent += countPlus;
+  totalEl.textContent = parseInt(totalEl.textContent) + count;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+function exited() {
+  if (count > parseInt(totalEl.textContent)) {
+    alert("Sorry, Exit Number can't be greater than Total Number.");
+    countEl.textContent = 0;
+    count = 0;
+
+  } else {
+    let countMinus = count + " - ";
+    exitEl.textContent += countMinus;
+    totalEl.textContent = parseInt(totalEl.textContent) - count;
+    countEl.textContent = 0;
+    count = 0;
+    
+  }
+}
+
+function resetAll() {
+  countEl.textContent = 0;
+  count = 0;
+  entryEl.textContent = entryEl.textContent.substring(0, 12);
+  exitEl.textContent = exitEl.textContent.substring(0, 11);
+  document.getElementById("totalSum").textContent = 0;
+}
